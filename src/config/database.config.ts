34d1803ad13ee39570dc192d0f0861286eb87c70
mengzhongda@foxmail.com <mengzhongda@foxmail.com>
2026-06-07 00:54:41 +0800

    first commit

---
+++ src/config/database.config.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, // 开发阶段自动同步表结构，生产环境请设为 false
+        charset: 'utf8mb4',
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
